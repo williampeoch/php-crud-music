ba8e48e450ff75df641e47d6fa6c713f8c79c4a4
William Peoc'h <william.peoch@etudiant.univ-reims.fr>
2023-05-23 16:02:01 +0200

    Push pendant la séance

--- public/cover.php
+++ public/cover.php
@@ -2,13 +2,18 @@
 
 declare(strict_types=1);
 
+use Entity\Cover;
 use Entity\Exception\EntityNotFoundException;
 use Entity\Exception\ParameterException;
 
 try {
-    if (!isset($_GET['coverId']) || !ctype_digit($_GET['coverId'])) {
-        throw new ParameterException();
+    if (!isset($_GET['coverId']) || !ctype_digit(intval($_GET['coverId']))) {
+        throw new ParameterException('Paramètre "coverId" manquant ou de mauvais type');
     }
+
+    $cover = Cover::findById(intval($_GET['coverId']))->getJpeg();
+    header('Content-Type: image/jpeg');
+    echo $cover;
 } catch (ParameterException) {
     http_response_code(400);
 } catch (EntityNotFoundException) {
